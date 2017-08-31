6edbcf9ba15dfa1b8e76816366878d2512aefa59
Rathi <github email address>
2017-08-31 09:05:10 -0400

    included site controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,28 @@
 class Api::V1::ItemsController < ApplicationController
+
   def index
+  @items = Item.all
+  respond_with(@items)
   end
 
-  def 
+  def create
+    Item.create(item_params)
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+  end
+
+  def update
+    item = Item.find(params["id"])
+    item.update_attributes(item_params)
+    respond_with item, json: item
+
+  end
+
+private
+
+  def item_params
+     params.require(:item).permit(:id, :name, :description)
+  end
 end
